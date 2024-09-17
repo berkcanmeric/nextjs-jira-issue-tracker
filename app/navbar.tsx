802a91ac2fe14aadee464a1e703e2dcf22ed1752
Berkcan Meric <berkcanmeric99@hotmail.com>
2024-09-17 22:43:34 +0300

    Style the active link in the navbar

---
+++ app/navbar.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+import { FaBug } from 'react-icons/fa';
+import classnames from 'classnames';
+
+const navbar = () => {
+  const currentPath = usePathname();
+  const links = [
+    { name: 'Dashboard', href: '/' },
+    { name: 'Issues', href: '/issues' },
+  ];
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+      <Link href='/'>
+        <FaBug />
+      </Link>
+      <ul className='flex space-x-6'>
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              className={classnames({
+                'text-zinc-900': link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true,
+              })}
+              href={link.href}
+            >
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default navbar;
