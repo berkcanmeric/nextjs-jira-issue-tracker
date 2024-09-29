17251ec8ae64b4100d5f00e3586c1701f055ef6d
Berkcan Meric <berkcanmeric99@hotmail.com>
2024-09-29 18:17:01 +0300

    Show issue details

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,22 @@
+import prisma from '@/prisma/client';
+import { notFound } from 'next/navigation';
+import React from 'react';
+
+const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) notFound();
+
+  return (
+    <div>
+      <p>{issue.title}</p>
+      <p>{issue.description}</p>
+      <p>{issue.status}</p>
+      <p>{issue.createdAt.toDateString()}</p>
+    </div>
+  );
+};
+
+export default IssueDetailPage;
