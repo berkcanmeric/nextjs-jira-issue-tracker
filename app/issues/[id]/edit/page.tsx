929c33501ec02717d600e0f1248ab66779be57d8
Berkcan Meric <berkcanmeric99@hotmail.com>
2024-10-05 23:11:57 +0300

    Build the edit issue page

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import prisma from '@/prisma/client';
+import IssueForm from '../../_components/IssueForm';
+import { notFound } from 'next/navigation';
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+interface Props {
+  params: { id: string };
 }
 
-export default page
+const EditIssuePage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue) notFound();
+  return <IssueForm issue={issue} />;
+};
+
+export default EditIssuePage;
