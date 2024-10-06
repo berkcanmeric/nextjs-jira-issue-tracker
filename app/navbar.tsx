13fdf828ca344941f323f7c18861c97bfa53e31f
Berkcan Meric <berkcanmeric99@hotmail.com>
2024-10-06 14:55:31 +0300

    Add a drop-down menu to show current user

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -6,7 +6,14 @@ import React from 'react';
 import { FaBug } from 'react-icons/fa';
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
-import { Box, DropdownMenu, Avatar, Flex, Container } from '@radix-ui/themes';
+import {
+  Box,
+  DropdownMenu,
+  Avatar,
+  Flex,
+  Container,
+  Text,
+} from '@radix-ui/themes';
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -16,7 +23,7 @@ const NavBar = () => {
     { name: 'Issues', href: '/issues/list' },
   ];
   return (
-    <nav className='  border-b mb-5 px-5 py-3 '>
+    <nav className='border-b mb-5 px-5 py-3'>
       <Container>
         <Flex justify='between'>
           <Flex align='center' gap='3'>
@@ -45,15 +52,25 @@ const NavBar = () => {
               <DropdownMenu.Root>
                 <DropdownMenu.Trigger>
                   <Avatar
-                    src='https://github.com/shadcn.png'
-                    fallback='?'
+                    src={session.user?.image || undefined}
+                    fallback={session.user?.name?.[0].toUpperCase() || '?'}
                     size='3'
                     radius='full'
+                    className='cursor-pointer'
                   />
                 </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
+                <DropdownMenu.Content sideOffset={5}>
+                  <DropdownMenu.Label>
+               
+                    <Text size='2' color='gray'>
+                      {session.user?.email}
+                    </Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Separator />
                   <DropdownMenu.Item>
-                    <Link href='/api/auth/signout'>Logout</Link>
+                    <Link href='/api/auth/signout' className='w-full'>
+                      Logout
+                    </Link>
                   </DropdownMenu.Item>
                 </DropdownMenu.Content>
               </DropdownMenu.Root>
