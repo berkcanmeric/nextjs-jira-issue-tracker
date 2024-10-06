5ca5f08a18075dac1133e964b35322ee63753ce4
Berkcan Meric <berkcanmeric99@hotmail.com>
2024-10-06 14:43:04 +0300

    Change the layout of the navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 import { FaBug } from 'react-icons/fa';
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
-import { Box, DropdownMenu, Avatar } from '@radix-ui/themes';
+import { Box, DropdownMenu, Avatar, Flex, Container } from '@radix-ui/themes';
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -16,48 +16,54 @@ const NavBar = () => {
     { name: 'Issues', href: '/issues/list' },
   ];
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-      <Link href='/'>
-        <FaBug />
-      </Link>
-      <ul className='flex space-x-6'>
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              className={classnames({
-                'text-zinc-900': link.href === currentPath,
-                'text-zinc-500': link.href !== currentPath,
-                'hover:text-zinc-800 transition-colors': true,
-              })}
-              href={link.href}
-            >
-              {link.name}
+    <nav className='  border-b mb-5 px-5 py-3 '>
+      <Container>
+        <Flex justify='between'>
+          <Flex align='center' gap='3'>
+            <Link href='/'>
+              <FaBug />{' '}
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === 'authenticated' && (
-          <DropdownMenu.Root>
-            <DropdownMenu.Trigger>
-              <Avatar
-                src='https://github.com/shadcn.png'
-                fallback='?'
-                size='3'
-                radius='full'
-              />
-            </DropdownMenu.Trigger>
-            <DropdownMenu.Content>
-              <DropdownMenu.Item>
-                <Link href='/api/auth/signout'>Logout</Link>
-              </DropdownMenu.Item>
-            </DropdownMenu.Content>
-          </DropdownMenu.Root>
-        )}
-        {status === 'unauthenticated' && (
-          <Link href='/api/auth/signin'>Login</Link>
-        )}
-      </Box>
+            <ul className='flex space-x-6'>
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    className={classnames({
+                      'text-zinc-900': link.href === currentPath,
+                      'text-zinc-500': link.href !== currentPath,
+                      'hover:text-zinc-800 transition-colors': true,
+                    })}
+                    href={link.href}
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === 'authenticated' && (
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src='https://github.com/shadcn.png'
+                    fallback='?'
+                    size='3'
+                    radius='full'
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Item>
+                    <Link href='/api/auth/signout'>Logout</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            )}
+            {status === 'unauthenticated' && (
+              <Link href='/api/auth/signin'>Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
